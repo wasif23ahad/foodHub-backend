ce9f7bfb3a86b3a78fa6d690bd7e34b0d8bc24be
wasif23ahad <wasifahad300@gmail.com>
2026-02-02 00:35:37 +0600

    feat: introduce Prisma schema for application models and initialize Prisma client with PostgreSQL adapter.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 
